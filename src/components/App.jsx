0cd3f8180a364dff79fadc7163b048a8a449ff41
thehsadowte <103116337+thehsadowte@users.noreply.github.com>
2022-12-29 13:01:46 +0200

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { getImages, normalizeImages } from 'services/api';
+
+import SearchBar from './SearchBar/SearchBar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Spinner from './Spinner/Spinner';
+
+export default class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    loading: false,
+    error: '',
+    totalImages: 0,
+  };
+
+  componentDidUpdate = async (_, prevState) => {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({ loading: true, error: '' });
+      try {
+        const { hits, totalHits } = await getImages(query, page);
+
+        const normalizedImages = normalizeImages(hits);
+
+        this.setState(prevState => {
+          return {
+            images: [...prevState.images, ...normalizedImages],
+            totalImages: totalHits,
+          };
+        });
+      } catch (error) {
+        this.setState({ error: 'something went wrong' });
+      } finally {
+        this.setState({ loading: false });
+      }
+    }
+
+    // if (prevState.error !== error && error) {
+    //   alert(error);
+    // }
+  };
+
+  getQuery = query => {
+    if (query === this.state.query) {
+      alert('same query, try change your request');
+      return;
+    }
+    this.setState({ query, images: [], page: 1 });
+  };
+
+  incrementPage = () => this.setState(prev => ({ page: prev.page + 1 }));
+
+  render() {
+    const { images, loading, totalImages, error } = this.state;
+    const showLoadMore = images.length !== totalImages && !loading;
+
+    return (
+      <div className="App">
+        <SearchBar onFormSubmit={this.getQuery} />
+
+        {images.length > 0 && <ImageGallery images={images} />}
+
+        {showLoadMore && <Button onClick={this.incrementPage} />}
+
+        {loading && <Spinner />}
+
+        {error && <p>{error}</p>}
+      </div>
+    );
+  }
+}
